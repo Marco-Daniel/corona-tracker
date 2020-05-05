de0f75db4d8d35128043d21b79dc296a23ab2475
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-05-05 16:47:37 +0200

    added particles to background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,34 @@
 import React, { useEffect, useState } from "react"
 import CircularProgress from "@material-ui/core/CircularProgress"
-import { useTheme } from "@material-ui/core/styles"
+import { useTheme, makeStyles } from "@material-ui/core/styles"
+import Particles from "react-particles-js"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import DataDisplay from "../components/dataDisplay"
 
+const useStyles = makeStyles(theme => ({
+  particles: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "98vw",
+    height: "98vh",
+    zIndex: -100,
+  },
+  body: {
+    width: "100%",
+    height: "95vh",
+    padding: `${theme.spacing(4)}px`,
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+}))
+
 const IndexPage = () => {
   const [data, setData] = useState([])
+  const classes = useStyles()
   const theme = useTheme()
 
   useEffect(() => {
@@ -33,17 +54,44 @@ const IndexPage = () => {
 
   return (
     <Layout>
-      <SEO title="Corona Tracker" />
-      <div
-        style={{
-          width: "100%",
-          height: "95vh",
-          padding: `${theme.spacing(4)}px`,
-          display: "flex",
-          alignItems: "flex-start",
-          justifyContent: "center",
+      <Particles
+        className={classes.particles}
+        params={{
+          particles: {
+            number: {
+              value: 15,
+              density: {
+                enable: true,
+                value_area: 500,
+              },
+            },
+            shape: {
+              type: "image",
+              image: { src: "covid-19.png" },
+            },
+            size: {
+              value: 10,
+              random: false,
+            },
+            opacity: {
+              value: 1,
+              random: true,
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              enable: true,
+              speed: 2.5,
+              random: true,
+              straight: false, // Whether they'll shift left and right while moving.
+              bounce: true,
+            },
+          },
         }}
-      >
+      />
+      <SEO title="Corona Tracker" />
+      <div className={classes.body}>
         {data.length === 0 ? <CircularProgress /> : <DataDisplay data={data} />}
       </div>
     </Layout>
