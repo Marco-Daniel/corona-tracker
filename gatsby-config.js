482167fd839cd54fdcfed9de9428ea6d0248c3cd
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-05-06 11:38:21 +0200

    changed flavicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         background_color: `#000000`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/covid-19.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
