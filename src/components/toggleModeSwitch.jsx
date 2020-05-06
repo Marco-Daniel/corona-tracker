6581fd5e1ece50b701bb376e51d1278b550357ca
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-05-06 11:22:12 +0200

    check if darkmode is prefered

--- src/components/toggleModeSwitch.jsx
+++ src/components/toggleModeSwitch.jsx
@@ -1,16 +1,22 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useContext, useEffect } from "react"
 import { Context } from "./contextProvider"
 import Switch from "@material-ui/core/Switch"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
 
 const ToggleModeSwitch = () => {
   const [checked, setChecked] = useState(false)
   const { useDarkMode, toggleDarkMode } = useContext(Context)
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
 
   const toggleChecked = () => {
     setChecked(prev => !prev)
     toggleDarkMode()
   }
+
+  useEffect(() => {
+    if (prefersDarkMode) toggleChecked()
+  }, [prefersDarkMode])
 
   return (
     <FormControlLabel
