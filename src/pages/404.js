914f8b8bd84cfc4361819b8151b43b47f4f048b4
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-05-11 15:50:16 +0200

    basic implementation of 404, redirect after 3 sec

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,42 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
+import { navigate, Link } from "gatsby"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  useEffect(() => {
+    if (window !== "undefined") {
+      setTimeout(() => {
+        navigate("/")
+      }, 3000)
+    }
+  }, [])
+
+  return (
+    <Layout>
+      <SEO title="404: Niet gevonden" />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <h1>Niet Gevonden</h1>
+        <p>
+          Deze app heeft geen subpagina's, u wordt nu terug gestuurd naar de
+          homepage.
+        </p>
+        <p>
+          Mocht het te lang duren of wordt u niet teruggestuurd dan kunt u{" "}
+          <Link to="/">hier</Link> klikken om terug te gaan naar de homepage.
+        </p>
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
