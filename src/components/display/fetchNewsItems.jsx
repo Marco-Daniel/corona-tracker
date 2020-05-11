01b2886601d0a344a761e386082a888f540511ee
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-05-11 18:12:14 +0200

    added fetch nos data

--- src/components/display/fetchNewsItems.jsx
+++ src/components/display/fetchNewsItems.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import CircularProgress from "@material-ui/core/CircularProgress"
-import DisplayNewsItems from "./displayNewsItems"
+import NewsTabs from "./newsTabs"
+import { xml2js } from "xml-js"
 
 import fetchNetworkResource from "../../globals/fetchNetworkResource"
+import capitalizeFirstLetter from "../../globals/capitalizeFirstLetter"
+
+const createSearchList = listArray => {
+  return listArray.reduce((accumulator, item) => {
+    accumulator.push(item, capitalizeFirstLetter(item))
+
+    return accumulator
+  }, [])
+}
 
 const FetchNewsItems = () => {
   const {
@@ -20,25 +30,76 @@ const FetchNewsItems = () => {
     }
   `)
 
-  const [data, setData] = useState(false)
+  const [rtlData, setRtlData] = useState(false)
+  const [nosData, setNosData] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     // useEffect can't be a async function of itself
     // so to do async work create a function inside of useEffect
     const asyncWork = async () => {
       try {
-        const url =
+        const rtlURL =
           "https://newsapi.org/v2/everything?sources=rtl-nieuws&q=+corona&sortBy=publishedAt"
-        const options = {
+        const rtlOptions = {
           method: "GET",
           headers: new Headers({
             Authorization: `Bearer ${newsAPIKey}`,
           }),
         }
 
-        const data = await fetchNetworkResource(url, options)
+        const nosURL =
+          "https://corsify.appspot.com/http://feeds.nos.nl/nosnieuwsbinnenland"
 
-        setData(data)
+        const [rtlData, nosData] = await Promise.all([
+          fetchNetworkResource(rtlURL, rtlOptions),
+          fetch(nosURL),
+        ])
+
+        const nosJSON = xml2js(await nosData.text(), {
+          alwaysChildren: true,
+          ignoreInstruction: true,
+          ignoreAttributes: true,
+          ignoreDeclaration: true,
+        })
+
+        const list = nosJSON.elements[0].elements[0].elements.filter(
+          el => el.name === "item"
+        )
+        const searchList = createSearchList([
+          "corona",
+          "coronatijd",
+          "pandemie",
+          "coronapandemie",
+          "corona-pandemie",
+          "besmetting",
+          "besmettingen",
+        ])
+
+        const coronaList = list.reduce((accumulator, item, i) => {
+          const title = item.elements.filter(el => el.name === "title")[0]
+            .elements[0].cdata
+          const description = item.elements.filter(
+            el => el.name === "description"
+          )[0].elements[0].text
+
+          let found = false
+
+          searchList.forEach((value, key) => {
+            const isInTitle = title.includes(value)
+            const isInDescription = description.includes(value)
+
+            if (isInTitle || isInDescription) found = true
+          })
+
+          if (found) accumulator.push(item)
+
+          return accumulator
+        }, [])
+
+        setNosData(coronaList)
+        setRtlData(rtlData)
+        setIsLoading(true)
       } catch (error) {
         console.log(error)
       }
@@ -47,8 +108,8 @@ const FetchNewsItems = () => {
     asyncWork()
   }, [])
 
-  return data ? (
-    <DisplayNewsItems data={data.articles} />
+  return isLoading ? (
+    <NewsTabs rtlData={rtlData} nosData={nosData} />
   ) : (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <CircularProgress color="secondary" />
